feba965ea474411e3ea1d967351fb09d4f064ea0
Luke Lin <lukelin1991@yahoo.com>
2019-12-08 16:54:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -88,9 +88,13 @@ def checkout(cart, coupons)
   total = 0
   counter = 0
   while counter < final_cart.length
-    total += final_cart[counter][:price]
+    total += final_cart[counter][:price] * final_cart[counter][:count]
     counter += 1
   end
+  if total > 100
+    total -= (total * 0.10)
+  end
+  return total
   # Consult README for inputs and outputs
   #
   # This method should call
