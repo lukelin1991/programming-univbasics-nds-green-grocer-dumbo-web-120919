5f27ed48f287f892b2d26fb19d7012fb00a09614
Luke Lin <lukelin1991@yahoo.com>
2019-12-08 16:24:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,6 +38,12 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  counter = 0
+  while counter < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    counter += 1
+  end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
